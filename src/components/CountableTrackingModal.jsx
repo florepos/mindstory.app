03fec430951bcab77b6bbf580555e334c4490189
florepos <florian.kathan@gmail.com>
2025-06-24 00:26:05 +0200

    Updated CountableTrackingModal.jsx

--- src/components/CountableTrackingModal.jsx
+++ src/components/CountableTrackingModal.jsx
@@ -131,7 +131,7 @@ const CountableTrackingModal = ({
                     </div>
                   )}
                 </div>
-              )}
+              </div>
             </div>
           )}
 
